c2efa0bc389a66c5248f87a2d49bdacbefc062ea
MohamedShadir <mohamed.shadir.uni@gmail.com>
2024-04-27 08:21:30 +0530

    Add layout and header

---
+++ layout/footer.php
@@ -0,0 +1,164 @@
+
+    <!-- Cta Area -->
+    <section id="cta_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7">
+                    <div class="cta_left">
+                        <div class="cta_icon">
+                            <img src="assets/img/common/email.png" alt="icon">
+                        </div>
+                        <div class="cta_content">
+                            <h4>Get the latest news and offers</h4>
+                            <h2>Subscribe to our newsletter</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="cat_form">
+                        <form id="cta_form_wrappper">
+                            <div class="input-group"><input type="text" class="form-control"
+                                    placeholder="Enter your mail address"><button class="btn btn_theme btn_md"
+                                    type="button">Subscribe</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer  -->
+    <footer id="footer_area" style="background:url('assets/icons/foot_bg.png');background-size:cover;background-repeat:no-repeat;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="footer_heading_area">
+                        <h5>Need any help?</h5>
+                    </div>
+                    <div class="footer_first_area">
+                        <div class="footer_inquery_area">
+                            <h3> <a href="tel:+020 3995 8555"> <span class="fas fa-phone"></span>  +020 3995 8555</a></h3>
+                        </div>
+                        <div class="footer_inquery_area">
+                            <h3> <a href="mailto:info@fareforyou.co.uk"><span class="fas fa-envelope"></span> info@fareforyou.co.uk</a></h3>
+                        </div>
+                        <div class="footer_inquery_area">
+                            <h5>Follow us on</h5>
+                            <ul class="soical_icon_footer">
+                                <li><a href="https://www.facebook.com/profile.php?id=100054443261877" class="fab fa-facebook fa-2x"></a></li>
+                                <li><a href="https://twitter.com/fareforyou" class="fab fa-twitter-square fa-2x"></a></li>
+                                <li><a href="https://www.instagram.com/fareforyou.co.uk/"  class="fab fa-instagram fa-2x"></a></li>
+                                <li><a href="https://www.linkedin.com/company/98472905" class="fab fa-linkedin fa-2x"></a></li>
+                            </ul>
+                        </div>
+                        <div class="footer_inquery_area">
+                            <h5> <a href="https://www.google.com/maps/search/?api=1&query=Unit%20313%20Barking%20Enterprise%20centre%2C%2050%20Cambridge%20Road%20Barking%20Greater%20London%20IG118FG" target="_blank"><span class="fas fa-map-marker-alt"></span> Unit313 Barking Enterprise Centre
+50 Cambridge Road
+Barking.
+</a></h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2  col-md-6 col-sm-6 col-12">
+                    <div class="footer_heading_area">
+                        <h5>Company</h5>
+                    </div>
+                    <div class="footer_link_area">
+                        <ul>
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                            <li><a href="#Blog">Blog</a></li>
+                            <li><a href="#Ebooks">Ebooks</a></li>
+                            <li><a href="#Sitemap">Sitemap</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="footer_heading_area">
+                        <h5>Support</h5>
+                    </div>
+                    <div class="footer_link_area">
+                        <ul>
+                            <li><a href="privacy-policy.php">Privacy Policy</a></li>
+                            <li><a href="privacy-policy.php?#cookies">Cookie Policy</a></li>
+                            <li><a href="terms-condition.php">Terms and conditions</a></li>
+                            <li><a href="assets/ATOLCertificate.pdf" target="_blank">AOTL certificate</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="footer_heading_area">
+                        <h5>Trending Flight dealz </h5>
+                    </div>
+                    <div class="footer_link_area">
+                        <ul>
+                            <li><a href="#privacy-policy.php">From India to Sri Lanka</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="footer_heading_area">
+                        <h5>Multi-Centre Holidays</h5>
+                    </div>
+                    <div class="footer_link_area">
+                        <ul>
+                            <li><a href="#privacy-policy.php">Bali</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> <br>
+            <div class="row">
+              <div class="col-lg-4">
+                <a href="assets/ATOLCertificate.pdf" target="_blank"><img src="assets/icons/atol-protected.png" style="width:100px;display:inline;" alt=""></a>
+                <a>  <img src="assets/icons/iata-logo-vector.svg" style="width:100px;display:inline;" alt=""> </a>
+               <a href="https://g.co/kgs/ktcQ98G" target="_blank">  <img src="assets/icons/google_review.png" style="width:150px;display:inline;" alt=""> </a>
+              </div>
+              <div class="col-lg-8">
+                <h5>All flights and packages booked through FareForYou for travel to and from the UK are covered by our ATOL 12362 protection from the Civil Aviation Authority (CAA).</h5>
+
+              </div>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright_area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="co-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="copyright_left">
+                        <p>Copyright © 2024 All rights reserved. FareForYou limited </p>
+                    </div>
+                </div>
+                <div class="co-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="copyright_left">
+                        <img src="icon/payment_link.png" style="width:100%;" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="go-top">
+        <i class="fas fa-chevron-up"></i>
+        <i class="fas fa-chevron-up"></i>
+    </div>
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <!-- Meanu js -->
+    <script src="assets/js/jquery.meanmenu.js"></script>
+    <!-- owl carousel js -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- wow.js -->
+    <script src="assets/js/wow.min.js"></script>
+    <!-- Custom js -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/add-form.js"></script>
+    <script src="assets/js/form-dropdown.js"></script>
+    <script src="assets/js/my.js?v=0.0001"></script>
+    <!-- TrustBox script -->
+<script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+<!-- End TrustBox script -->
+</body>
+
+</html>
